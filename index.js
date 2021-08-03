35e0d1ca6d0270d4de4099593a244672e53c0684
Leon Steinmeier <lenostatos@gmx.de>
2021-08-03 09:27:05 +0200

    part3-phonebook: Do exercise 3.22.

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 require('dotenv').config();
 
+const process = require('process');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const Entry = require('./models/entry');
-
-/**
- * Indicates whether an object is emtpy, i.e. equivalent to {}.
- * 
- * Implementation based on https://stackoverflow.com/questions/679915/how-do-i-test-for-an-empty-javascript-object/59787784#59787784
- */
-function isEmpty(obj) {
-    for (prop in obj) { return false; }
-    return obj.constructor === Object;
-}
 
 const app = express();
 
@@ -30,9 +21,9 @@ morgan.token('request-body', request => JSON.stringify(request.body));
 app.post('*', morgan('request body: :request-body'));
 
 app.get('/info', async (request, response) => {
-    const entryCount = await Entry.countDocuments({});
-    const currentDate = new Date();
-    response.send(`
+   const entryCount = await Entry.countDocuments({});
+   const currentDate = new Date();
+   response.send(`
         <p>The phonebook has ${entryCount} entries.</p>
         <p>${currentDate.toDateString()} ${currentDate.toTimeString()}</p>
     `);
@@ -40,93 +31,93 @@ app.get('/info', async (request, response) => {
 
 // Get all entries
 app.get('/api/persons', async (request, response) => {
-    const entries = await Entry.find({});
-    response.json(entries);
+   const entries = await Entry.find({});
+   response.json(entries);
 });
 
 // Post a new entry
 app.post('/api/persons', async (request, response, next) => {
-    const requestBody = request.body;
-    
-    const postedEntry = new Entry({
-        name: requestBody.name,
-        number: requestBody.number
-    });
+   const requestBody = request.body;
 
-    try {
-        const savedEntry = await postedEntry.save();
-        response.json(savedEntry);
-    } catch (error) {
-        console.log('error while saving a new entry');
-        next(error);
-    }
+   const postedEntry = new Entry({
+      name: requestBody.name,
+      number: requestBody.number
+   });
+
+   try {
+      const savedEntry = await postedEntry.save();
+      response.json(savedEntry);
+   } catch (error) {
+      console.log('error while saving a new entry');
+      next(error);
+   }
 });
 
 // Handle requests around single, existing entries
 app.route('/api/persons/:id')
-    .get(async (request, response, next) => {
+   .get(async (request, response, next) => {
 
-        let requestedEntry;
-        try {
-            requestedEntry = await Entry.findById(request.params.id);
-        } catch (error) {
-            console.log('error while searching for an entry by id');
-            next(error);
-        }
+      let requestedEntry;
+      try {
+         requestedEntry = await Entry.findById(request.params.id);
+      } catch (error) {
+         console.log('error while searching for an entry by id');
+         next(error);
+      }
 
-        if (requestedEntry) {
-            response.json(requestedEntry);
-        } else {
-            response.status(404).end();
-        }
-    })
-    .put(async (request, response, next) => {
-        
-        try {
-            // mongoose recommends doing updates as follows instead of using, 
-            // e.g. findByIdAndUpdate: 
-            // https://mongoosejs.com/docs/documents.html#updating-using-queries
-            const entryOnServer = await Entry.findById(request.params.id);
-            // only ever update the number
-            entryOnServer.number = request.body.number;
-            const updatedEntryOnServer = await entryOnServer.save();
-            response.json(updatedEntryOnServer);
-        } catch (error) {
-            console.log('error while updating an entry');
-            next(error);
-        }
-    })
-    .delete(async (request, response) => {
-        try {
-            await Entry.findByIdAndDelete(request.params.id);
-        } catch (error) {
-            console.log('error while deleting an entry:', error);
-        }
+      if (requestedEntry) {
+         response.json(requestedEntry);
+      } else {
+         response.status(404).end();
+      }
+   })
+   .put(async (request, response, next) => {
 
-        response.status(204).end();
-    });
+      try {
+         // mongoose recommends doing updates as follows instead of using,
+         // e.g. findByIdAndUpdate:
+         // https://mongoosejs.com/docs/documents.html#updating-using-queries
+         const entryOnServer = await Entry.findById(request.params.id);
+         // only ever update the number
+         entryOnServer.number = request.body.number;
+         const updatedEntryOnServer = await entryOnServer.save();
+         response.json(updatedEntryOnServer);
+      } catch (error) {
+         console.log('error while updating an entry');
+         next(error);
+      }
+   })
+   .delete(async (request, response) => {
+      try {
+         await Entry.findByIdAndDelete(request.params.id);
+      } catch (error) {
+         console.log('error while deleting an entry:', error);
+      }
+
+      response.status(204).end();
+   });
 
 app.use((request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' });
+   response.status(404).send({ error: 'unknown endpoint' });
 });
 
 function errorHandler(error, request, response, next) {
-    console.error(error);
+   console.error(error);
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformed id' });
-    }
+   if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformed id' });
+   }
 
-    if (error.name === 'ValidationError') {
+   if (error.name === 'ValidationError') {
       return response.status(400).json({ error: error.message });
-    }
+   }
 
-    next(error);
+   next(error);
 }
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}...`);
+   console.log(`Server running on port ${PORT}...`);
 });
--- models/entry.js
+++ models/entry.js
@@ -1,3 +1,4 @@
+const process = require('process');
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
@@ -6,42 +7,42 @@ const url = process.env.MONGODB_URI;
 console.log('connecting to', url);
 
 (async function() {
-    try {
-        await mongoose.connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-        });
-        console.log('connected to MongoDB');
-    } catch (error) {
-        console.log('error connecting to MongoDB:', error.message);
-    }
+   try {
+      await mongoose.connect(url, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useFindAndModify: false,
+         useCreateIndex: true
+      });
+      console.log('connected to MongoDB');
+   } catch (error) {
+      console.log('error connecting to MongoDB:', error.message);
+   }
 })();
 
 const entrySchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 3,
-        unique: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true
-    }
+   name: {
+      type: String,
+      required: true,
+      minLength: 3,
+      unique: true
+   },
+   number: {
+      type: String,
+      minLength: 8,
+      required: true
+   }
 });
 
 entrySchema.plugin(uniqueValidator);
 
 entrySchema.set('toJSON', {
-  transform: (document, convertedDocument) => {
-    convertedDocument.id = convertedDocument._id.toString();
-    delete convertedDocument._id;
-    delete convertedDocument.__v;
-    return convertedDocument;
-  }
+   transform: (document, convertedDocument) => {
+      convertedDocument.id = convertedDocument._id.toString();
+      delete convertedDocument._id;
+      delete convertedDocument.__v;
+      return convertedDocument;
+   }
 });
 
 module.exports = mongoose.model('Entry', entrySchema);
